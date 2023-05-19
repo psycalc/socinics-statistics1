6f511fde5b3cd26ecb7b9fc08cf3f4dda9d5b803
Oleh <designofhumanity@gmail.com>
2023-05-19 23:04:56 +0300

    біль реалістичний приклад, аналіз

--- socionics_analyze.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-
-df = pd.read_csv('знаменитості.csv')
-df['is_match'] = df['expert_type'] == df['new_method_type']
-
-match_percentage = df['is_match'].sum() / len(df) * 100
-print(f'Відсоток збігів: {match_percentage}%')
--- socionics_statistics.py
+++ socionics_statistics.py
@@ -1,37 +1,29 @@
 import pandas as pd
 from slang import slang
 
-def create_df(respondent_ids, full_names, expert_types, new_method_types, socionics_schools, typing_methods):
-    return pd.DataFrame({
-        'respondent_id': respondent_ids,
-        'full_name': full_names,
-        'expert_type': expert_types,
-        'new_method_type': new_method_types,
-        'socionics_school': socionics_schools,
-        'typing_method': typing_methods
-    })
+def read_csv(filename):
+    return pd.read_csv(filename)
 
-full_names = ["Зеленський Володимир Олександрович", "Порошенко Петро Олексійович", "Ющенко Віктор Андрійович", "Кучма Леонід Данилович", "Кравчук Леонід Макарович"]
-respondent_ids = range(1, len(full_names) + 1)
-expert_types = ['INTJ', 'ENFP', 'ISTP', 'ESFJ', 'ENTJ']
-new_method_types = ['INTJ', 'ENFP', 'ISTP', 'ESFJ', 'ENTJ']
-socionics_schools = ['Школа 1', 'Школа 2', 'Школа 3', 'Школа 4', 'Школа 5']
-typing_methods = ['Метод 1', 'Метод 2', 'Метод 3', 'Метод 4', 'Метод 5']
+def analyze(df):
+    df['is_match'] = df['expert_type'] == df['new_method_type']
+    
+    # Групування по школі
+    school_group = df.groupby('socionics_school')['is_match'].mean() * 100
+    print('Відсоток збігів по школах:\n', school_group)
 
-df = create_df(respondent_ids, full_names, expert_types, new_method_types, socionics_schools, typing_methods)
+    # Групування по типувальнику
+    typist_group = df.groupby('типувальник')['is_match'].mean() * 100
+    print('Відсоток збігів по типувальникам:\n', typist_group)
 
-# Створюємо DataFrame для шкіл соціоніки та їх методів типування
-school_typing_methods = {
-    'Школа 1': ['Метод 1', 'Метод 2'],
-    'Школа 2': ['Метод 3', 'Метод 4'],
-    'Школа 3': ['Метод 5', 'Метод 6'],
-    'Школа 4': ['Метод 7', 'Метод 8'],
-    'Школа 5': ['Метод 9', 'Метод 10']
-}
 
-schools_df = pd.DataFrame(list(school_typing_methods.items()), columns=['socionics_school', 'typing_methods'])
+# Читаємо дані з файлів
+celebrities_df = read_csv('знаменитості.csv')
+socionics_schools_df = read_csv('школи_соціоніки.csv')
+typing_methods_df = read_csv('методи_типування.csv')
 
-# З'єднуємо два DataFrame
-df = pd.merge(df, schools_df, on='socionics_school', how='left')
+# З'єднуємо дані за потрібними стовпцями
+df = pd.merge(celebrities_df, socionics_schools_df, on='socionics_school', how='left')
+df = pd.merge(df, typing_methods_df, on='typing_method', how='left')
 
-df.to_csv('знаменитості.csv', index=False)
+# Аналізуємо дані
+analyze(df)
