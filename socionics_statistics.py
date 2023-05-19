d1d7277273110e0510e9db7167a4437d14e24b9e
Oleh <designofhumanity@gmail.com>
2023-05-19 22:29:48 +0300

    another fix

--- socionics_statistics.py
+++ socionics_statistics.py
@@ -3,9 +3,6 @@ from slang import slang
 
 full_names = ["Зеленський Володимир Олександрович", "Порошенко Петро Олексійович", "Ющенко Віктор Андрійович", "Кучма Леонід Данилович", "Кравчук Леонід Макарович"]
 
-
-
-
 respondent_ids = range(1, len(full_names) + 1)
 expert_types = ['INTJ', 'ENFP', 'ISTP', 'ESFJ', 'ENTJ']
 new_method_types = ['INTJ', 'ENFP', 'ISTP', 'ESFJ', 'ENTJ']
