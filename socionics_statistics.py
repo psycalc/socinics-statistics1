842f4c3b88f6a722be794d334440ef698ba69f3b
Oleh <designofhumanity@gmail.com>
2023-05-19 23:17:03 +0300

    another changes

--- socionics_statistics.py
+++ socionics_statistics.py
@@ -1,29 +1,19 @@
+# pandas
 import pandas as pd
-from slang import slang
+# Завантажуємо дані з файлів
+celebrities_df = pd.read_csv('celebrities.csv')
+schools_df = pd.read_csv('школи_соціоніки.csv')
+typists_df = pd.read_csv('типувальники.csv')
 
-def read_csv(filename):
-    return pd.read_csv(filename)
+# Об'єднуємо дані
+df = pd.concat([schools_df, typists_df])
 
-def analyze(df):
-    df['is_match'] = df['expert_type'] == df['new_method_type']
-    
-    # Групування по школі
-    school_group = df.groupby('socionics_school')['is_match'].mean() * 100
-    print('Відсоток збігів по школах:\n', school_group)
+# Аналізуємо збіги між експертами та новим методом
+df['is_match'] = df['typed_type'].fillna('') == df['typing_method'].fillna('')
 
-    # Групування по типувальнику
-    typist_group = df.groupby('типувальник')['is_match'].mean() * 100
-    print('Відсоток збігів по типувальникам:\n', typist_group)
+# Розраховуємо відсоток збігів за школами та типувальниками
+school_match_percentages = df.groupby('socionics_school')['is_match'].mean() * 100
+typist_match_percentages = df.groupby('typist')['is_match'].mean() * 100
 
-
-# Читаємо дані з файлів
-celebrities_df = read_csv('знаменитості.csv')
-socionics_schools_df = read_csv('школи_соціоніки.csv')
-typing_methods_df = read_csv('методи_типування.csv')
-
-# З'єднуємо дані за потрібними стовпцями
-df = pd.merge(celebrities_df, socionics_schools_df, on='socionics_school', how='left')
-df = pd.merge(df, typing_methods_df, on='typing_method', how='left')
-
-# Аналізуємо дані
-analyze(df)
+print(f'Відсоток збігів по школах:\n{school_match_percentages}')
+print(f'Відсоток збігів по типувальникам:\n{typist_match_percentages}')
